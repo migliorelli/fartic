d5f5554a26bb3670bea281a5f63863317c8e2e97
Miguel Migliorelli Bringhenti <miglenti@gmail.com>
2025-02-13 08:15:15 -0300

    error middleware refactor

--- backend/src/middlewares/error.middleware.ts
+++ backend/src/middlewares/error.middleware.ts
@@ -1,24 +1,26 @@
 import { NextFunction, Request, Response } from "express";
 import HttpError from "../errors/http.error";
 
-const errorMiddleware = (
-  error: HttpError,
-  req: Request,
-  res: Response,
-  next: NextFunction,
-) => {
-  try {
-    const status = error.status || 500;
-    const message = error.message || "Something went wrong.";
+const errorMiddleware = () => {
+  return (
+    error: HttpError,
+    req: Request,
+    res: Response,
+    next: NextFunction,
+  ) => {
+    try {
+      const status = error.status || 500;
+      const message = error.message || "Something went wrong.";
 
-    console.error(
-      `${req.method} - ${req.path} => Status: ${status}, Message: ${message}`,
-    );
+      console.error(
+        `${req.method} - ${req.path} => Status: ${status}, Message: ${message}`,
+      );
 
-    res.status(status).json({ message });
-  } catch (error) {
-    next(error);
-  }
+      res.status(status).json({ message });
+    } catch (error) {
+      next(error);
+    }
+  };
 };
 
 export default errorMiddleware;
