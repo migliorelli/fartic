7cd45beeca1459b690fe47eafa7ef0d1036a9355
Miguel Migliorelli Bringhenti <miglenti@gmail.com>
2025-02-14 17:21:50 -0300

    using isEmpty

--- backend/src/validators/player.validator.ts
+++ backend/src/validators/player.validator.ts
@@ -1,5 +1,11 @@
+import { isEmpty } from "@/utils/util";
+
 export function validateCreatePlayer(player: any) {
-  if (typeof player !== "object" || !player.socketId || !player.username)
+  if (
+    typeof player !== "object" ||
+    isEmpty(player.socketId) ||
+    isEmpty(player.username)
+  )
     return false;
 
   return true;
