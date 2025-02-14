f9f639c589db08d49e1453da53029714a8930411
Miguel Migliorelli Bringhenti <miglenti@gmail.com>
2025-02-14 17:24:28 -0300

    added theme routes to app

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,6 +2,14 @@ import App from "./app";
 import PingRoutes from "./routes/ping.routes";
 import PlayerRoutes from "./routes/player.routes";
 import RoomRoutes from "./routes/room.routes";
+import ThemeRoutes from "./routes/theme.routes";
 
-const app = new App([new PingRoutes(), new RoomRoutes(), new PlayerRoutes()]);
+const routes = [
+  new PingRoutes(),
+  new RoomRoutes(),
+  new PlayerRoutes(),
+  new ThemeRoutes(),
+];
+
+const app = new App(routes);
 app.listen();
