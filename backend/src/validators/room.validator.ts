f7a78ca2ee104ebd120939436ad4ebcc1da09aa2
Miguel Migliorelli Bringhenti <miglenti@gmail.com>
2025-02-15 16:35:46 -0300

    added theme, targetPontuation and playerLimit to validation

--- backend/src/validators/room.validator.ts
+++ backend/src/validators/room.validator.ts
@@ -1,4 +1,12 @@
+import { isEmpty } from "@/utils/util";
+
 export function validateCreateRoom(room: any) {
-  if (typeof room !== "object") return false;
+  if (
+    typeof room !== "object" ||
+    isEmpty(room.theme) ||
+    isEmpty(room.targetPontuation) ||
+    isEmpty(room.playerLimit)
+  )
+    return false;
   return true;
 }
