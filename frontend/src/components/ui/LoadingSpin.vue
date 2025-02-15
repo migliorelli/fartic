bfa68f1ec229aeae71b8867d8960d9cbc922eb83
Miguel Migliorelli Bringhenti <miglenti@gmail.com>
2025-02-15 15:21:08 -0300

    added class prop to LoadingSpin

--- frontend/src/components/ui/LoadingSpin.vue
+++ frontend/src/components/ui/LoadingSpin.vue
@@ -1,5 +1,21 @@
+<script setup lang="ts">
+import { twMerge } from "tailwind-merge";
+
+interface Props {
+  class?: string;
+}
+
+const props = defineProps<Props>();
+</script>
+
 <template>
   <div
-    class="h-10 w-10 animate-spin rounded-full border-4 border-violet-600 border-t-transparent"
+    :class="
+      twMerge(
+        'size-10 animate-spin rounded-full border-4 border-solid border-violet-600',
+        props.class,
+        'border-t-transparent',
+      )
+    "
   ></div>
 </template>
