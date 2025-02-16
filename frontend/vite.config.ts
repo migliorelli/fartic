080c4c71fd78057a71afdacaf16a2e8640f2d5b8
Miguel Migliorelli Bringhenti <miglenti@gmail.com>
2025-02-15 22:46:26 -0300

    using env var on proxy target

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,19 +1,23 @@
 import tailwindcss from "@tailwindcss/vite";
 import vue from "@vitejs/plugin-vue";
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [vue(), tailwindcss()],
-  server: {
-    proxy: {
-      "/api": {
-        target: "http://localhost:5174/v1",
-        changeOrigin: true,
-        ws: true,
-        secure: false,
-        rewrite: (path) => path.replace(/^\/api/, ""),
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
+
+  return {
+    plugins: [vue(), tailwindcss()],
+    server: {
+      proxy: {
+        "/api": {
+          target: `http://localhost:5174/${env.VITE_API_VERSION}`,
+          changeOrigin: true,
+          ws: true,
+          secure: false,
+          rewrite: (path) => path.replace(/^\/api/, ""),
+        },
       },
     },
-  },
+  };
 });
