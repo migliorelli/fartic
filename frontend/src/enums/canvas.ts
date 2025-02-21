c628ecc75e09159702c358c3ff78283ce91cd306
Miguel Migliorelli Bringhenti <miglenti@gmail.com>
2025-02-20 22:52:03 -0300

    created canvas base

---
+++ frontend/src/enums/canvas.ts
@@ -0,0 +1,14 @@
+enum StrokeType {
+  Eraser = "ERASER",
+  Dash = "DASH",
+
+  Square = "SQUARE",
+  Circle = "CIRCLE",
+
+  Triangle = "TRIANGLE",
+  HalfTriangle = "HALFTRIANGLE",
+
+  Line = "LINE",
+}
+
+export default StrokeType;
